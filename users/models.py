3c9f8f8e547bfa139fbd4bd3eb413a2407af7824
Mark Volkov <markasch67@gmail.com>
2025-09-26 20:38:06 +0300

    User models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
-# from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    """Класс для пользователей системы."""
+
+    email = models.EmailField(unique=True, verbose_name="Email")
+    first_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Имя")
+    last_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Фамилия")
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        ordering = ["first_name", "last_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
